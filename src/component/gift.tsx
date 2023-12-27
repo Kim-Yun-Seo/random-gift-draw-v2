b497c8c7e8ee62f88648a1b5fcd9551169b0f6bd
Kim-Yun-Seo <rlagrace410@gmail.com>
2023-12-28 02:41:01 +0900

    [feat] 랜덤 뽑기 기능 완료, 이미지 변경 기능 일부 완료

--- src/component/gift.tsx
+++ src/component/gift.tsx
@@ -4,19 +4,25 @@ import React from 'react';
 import styles from '../component/gift.module.css'
 
 export const Gift = () => {
-  const movePage = useNavigate();
-  const homeClick = () => { movePage('/') }
+  // const movePage = useNavigate();
+  // const homeClick = () => { movePage('/') }
+  // const backClick = () => { movePage('/rule')}
   const [giftList, setGiftList] = useState(['a', 'b', 'c', 'd', 'e', 'f']) //여기 선물도 랜덤으로 배열
   const [peopleNum, setPeopleNum] = useState(0)
-  const [nowPerson, setNowPerson] = useState('/person1.png') //Ready img로 시작
-  const [nowGift, setNowGift] = useState('/person1.png') //Ready img로 시작
-  let [randomPeople, setRandomPeople] = useState([]) //canGet할 수 있는 사람들이 모여있는 배열
+  const [nowPerson, setNowPerson] = useState('/person_ready.svg')
+  const [nowGift, setNowGift] = useState('/present_ready.png')
+  let [randomPeople, setRandomPeople] = useState([])
   // useEffect(()=>{
   //   console.log('nowPerson =' , nowPerson)
   // },[nowPerson]); 
 
   const openPerson = () => {
-    setNowPerson('/b.jpeg')
+    setNowPerson('/a.jpeg')
+  }
+
+  const changeGiftImg = () => {
+    //버튼 누르면 사진 이미지 바뀌기
+    setNowGift('/b.jpeg')
   }
 
   const giftImg = {
@@ -46,74 +52,82 @@ export const Gift = () => {
       },
       {
         "name": "yun",
-        "canGet": ['a', 'b','e', 'f'],
+        "canGet": ['a', 'b','c', 'd'],
         "gift": ['e', 'f'],
         'count': 0,
         "img": ''
       }
     ]
   )
-
-  //giftList[0]을 가지고 할거야.
-
-  info.forEach((i, idx) => {
-    console.log(' i.name' , i.name ,i.canGet.includes(giftList[0]))
-    //randomPeople중에 하나를 랜덤으로 뽑아서.
-    //nowPerson을 바꿔준다.
-    if (i.canGet.includes(giftList[0])) {
-      randomPeople.push(i.name)
-    }
-    console.log(' =' , randomPeople)
-  })
-
-  function getRandomInt(min, max) {
+  const getRandomInt = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //최댓값도 포함, 최솟값도 포함
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  const personPick = () => {
+    randomPeople = [];
+
+    info.forEach((i, idx) => {
+      console.log('name =' , i.name )
+      if (i.canGet.includes(giftList[0]) && i.count < 2) {
+        randomPeople.push(i.name)
+      }
+      console.log('randomList =' , randomPeople)
+    })
+    const randomInt = getRandomInt(0,randomPeople.length - 1);
+    console.log('randomInt =' , randomInt)
+
+    info.forEach((i, idx) => {
+      if (i.name === randomPeople[randomInt]) {
+        i.count++
+        console.log('당첨자 =' ,randomPeople[randomInt], i.count )
+      }
+    })
+    giftList.shift()
+    console.log('gL =' , giftList)
+    console.log('randomPeople =' , randomPeople)
+    //사진 바꾸기를 해야합니다.
+    //인물 사진 바꾸기
+    //그리고 완료되면 다시 ready.png로 바꾸는 작업
   }
 
-  getRandomInt(0,randomPeople.length - 1)
-  //랜덤 숫자 뽑아서,
-  //RandomPeople[getRandonInt()]
-  console.log('slslsl =' , randomPeople[getRandomInt(0,randomPeople.length - 1)])
-  //여기까지 하고
-  //randomPeople 은 reset.
-
-  giftList.shift()
-  randomPeople = [];
-  console.log('gL =' , giftList)
-  console.log('randomPeople =' , randomPeople)
   return (
     <>
-      
-      <div className={styles}>
-        {peopleNum}
-      </div>
       <div className={styles.title}>
-        Merry Christmas
+        Happy New Year
       </div>
       <div className={styles.subTitle}>
         random-gift-draw   
       </div>
-
       <div className={styles.items}>
+        <img className={styles.gift}
+          src={nowGift}>
+        </img>
         <img className={styles.person}
         src={nowPerson}>
         </img>
-        <img className={styles.gift}
-        src={giftImg.a}>
-        </img>
       </div>
-      <button className={styles.startBtn} onClick={() => {
-        console.log('start 다음 페이지로 이동 =' , )
-        openPerson()
-        // 사람 사진 변경
-        // 선물 사진 변경
-        // 노래 재생
-        // 효과 재생
-      }}>
-        START
-      </button>
+
+      <div className={styles.buttons}>
+        <button className={styles.startBtn} onClick={() => {
+            console.log('start 다음 페이지로 이동 =' , )
+            // backClick()
+            changeGiftImg()
+            //선물사진변경
+          }}>
+          선물뽑기
+        </button>
+        <button className={styles.startBtn} onClick={() => {
+          console.log('pick start' , )
+          openPerson()
+          personPick()
+          // 사람 사진 변경
+          // 노래 재생
+          // 효과 재생
+        }}>
+          사람뽑기
+        </button>
+      </div>
     </>
   );
 }
--- src/component/start.tsx
+++ src/component/start.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import React from 'react';
 import styles from '../component/start.module.css';
@@ -6,58 +5,34 @@ import styles from '../component/start.module.css';
 export const Start = () => {
   const movePage = useNavigate();
   const homeClick = () => { movePage('/rule') }
-  const [giftList, setGiftList] = useState(['a', 'b', 'c', 'd', 'e', 'f']) //여기 선물도 랜덤으로 진행
-  const [peopleNum, setPeopleNum] = useState(0)
-  const [info, setInfo] = useState(
-    [
-      {
-        "name": "kim",
-        "canGet": ['c', 'd', 'e', 'f'],
-        "gift": ['a', 'b'],
-        'count': 0
-      },
-      {
-        "name": "lee",
-        "canGet": ['a', 'b','e', 'f'],
-        "gift": ['c', 'd'],
-        'count': 0
-      },
-      {
-        "name": "yun",
-        "canGet": ['a', 'b','e', 'f'],
-        "gift": ['e', 'f'],
-        'count': 0
-      }
-    ]
-  )
-  giftList.forEach(g => {
-    console.log('nowGift =' , g)
-  });
-  //이건 input 받아서 채워넣을 것
-  console.log('info =' , info)
+
   return (
     <>
-      <div className={styles}>
-        {peopleNum}
-      </div>
       <div className={styles.title}>
-        Merry Christmas
+        Happy New Year
       </div>
       <div className={styles.subTitle}>
         random-gift-draw   
       </div>
 
-      <div style={{marginLeft: "40%"}}>
-        <input type='text' placeholder='ex) 2명' className={styles.info}/>
-        <input type='text' placeholder='ex) 선물' className={styles.info}/>
+      <div className={styles.etc}>
+        고생한 우리 모두와 <br />
+        새로운 한 해를 위해 <br />
+        <br />
+        2024년 힘차게 파이팅! <br />
+        <br />
+        2023년 마무리를 즐겁게!
+        
       </div>
 
-      <button className={styles.startBtn} onClick={() => {
-        console.log('start 다음 페이지로 이동 =' , )
-        homeClick()
-      }}>
-        START
-      </button>
+      <div className={styles.buttons}>
+        <button className={styles.startBtn} onClick={() => {
+          console.log('start 다음 페이지로 이동 =' , )
+          homeClick()
+        }}>
+          START
+        </button>
+      </div>
     </>
   );
 }
